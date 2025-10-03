c62c1be698a548e5302912698a3b8bc5d1e8fb87
cjbarre <cjbarre@gmail.com>
2025-10-03 07:54:32 -0700

    Basic rendering of grid floor from gltf file.

--- shaders/basic_fragment.glsl
+++ shaders/basic_fragment.glsl
@@ -1,16 +1,25 @@
 #version 330 core
-
 out vec4 FragColor;
-
-in vec3 ourColor;
-in vec2 TexCoord;
-
-uniform sampler2D texture1;
-uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = mix(texture(texture1, TexCoord),
-                    texture(texture2, TexCoord),
-                    0.2);
+    FragColor = vec4(0.8, 0.8, 0.8, 1.0); // solid gray
 }
+
+
+// #version 330 core
+
+// out vec4 FragColor;
+
+// in vec3 ourColor;
+// in vec2 TexCoord;
+
+// uniform sampler2D texture1;
+// uniform sampler2D texture2;
+
+// void main()
+// {
+//     FragColor = mix(texture(texture1, TexCoord),
+//                     texture(texture2, TexCoord),
+//                     0.2);
+// }
