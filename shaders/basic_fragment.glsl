f1836bad1ba34329f0eca77e62b3e39f286be4b3
cjbarre <cjbarre@gmail.com>
2025-10-04 09:27:50 -0700

    Basic textured floor plane rendered from GLTF model file.

--- shaders/basic_fragment.glsl
+++ shaders/basic_fragment.glsl
@@ -1,25 +1,29 @@
-#version 330 core
-out vec4 FragColor;
-
-void main()
-{
-    FragColor = vec4(0.8, 0.8, 0.8, 1.0); // solid gray
-}
-
-
 // #version 330 core
-
 // out vec4 FragColor;
-
-// in vec3 ourColor;
-// in vec2 TexCoord;
-
-// uniform sampler2D texture1;
-// uniform sampler2D texture2;
 
 // void main()
 // {
-//     FragColor = mix(texture(texture1, TexCoord),
-//                     texture(texture2, TexCoord),
-//                     0.2);
+//     FragColor = vec4(0.8, 0.8, 0.8, 1.0); // solid gray
 // }
+
+
+#version 330 core
+
+in vec2 TexCoord;
+
+out vec4 FragColor;
+
+uniform sampler2D uBaseColorTex;
+uniform vec4 uBaseColorFactor;
+uniform bool uHasBaseColorTex; // flag in case there’s no texture
+
+void main()
+{
+    vec4 baseColor = uBaseColorFactor;
+
+    if (uHasBaseColorTex) {
+        baseColor *= texture(uBaseColorTex, TexCoord);
+    }
+
+    FragColor = baseColor;
+}
